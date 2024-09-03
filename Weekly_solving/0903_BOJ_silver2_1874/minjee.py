088062aaba270bc73ef2b7176e4b754f0413f600
minjeeki <minjeeki@student.42seoul.kr>
2024-09-03 09:14:38 +0900

    [BOJ 1874 스택수열] 타인 코드 업로드

--- Weekly_solving/0903_BOJ_silver2_1874/minjee.py
+++ Weekly_solving/0903_BOJ_silver2_1874/minjee.py
@@ -42,3 +42,32 @@ if can_make:
 else:
     print('NO')
 
+''' jisulala7 코드
+import sys
+n = int(sys.stdin.readline())
+stack=[]
+answer = []
+now = 1
+FT = True
+for i in range(n):
+    num = int(sys.stdin.readline())
+    # 스택 쌓기
+    while now <= num:
+        stack.append(now)
+        answer.append("+")
+        now += 1
+    # 스택 꺼내기
+    if stack[-1] == num:
+        stack.pop()
+        answer.append("-")
+    # 불가능한 경우
+    else:
+        FT = False
+
+# 스택 수열 만들수 있는지 여부에 따라 출력
+if FT:
+    for ans in answer:
+        print(ans)
+else:
+    print('NO')
+'''
