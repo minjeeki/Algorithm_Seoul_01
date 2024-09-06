1d80da37820ac217149d222a7bf8ee6d22b76820
minjeeki <minjeeki@student.42seoul.kr>
2024-09-06 13:45:37 +0900

    [BOJ 14501 퇴사] 타인 코드 이해

--- Weekly_solving/0906_BOJ_silver3_14501/minjee.py
+++ Weekly_solving/0906_BOJ_silver3_14501/minjee.py
@@ -1,15 +1,50 @@
-'''
-* 주요 아이디어 : 
-* 생각한 알고리즘 : 백트래킹, 재귀 / 싫다면 노가다 (브루트포스, 완전 탐색)
+# 다음에 코드 풀 것임
+
+
+'''DP : bottom-up'''
+import sys
+N = int(input())
+
+# 사용자 입력 받기
+schedule  = [list(map(int, input().split())) for i in range(N)]
+
+dp = [0 for i in range(N + 1)]
+
+for i in range(N):
+    for j in range(i + schedult[i][0], N + 1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+print(dp[-1])
 
 '''
+DP : top-down
+뒤에서부터 접근 & 점화식 세우기
+'''
+N = int(input())
+li = [list(map(int, input().split())) for _ in range(N)]
+dp = [0 for _ in range(N + 1)]
 
-N = int(input())            # N : 상담 받을 수 있는 일자 개수
-work_lst = [[]] * (N + 1)   # work_lst : 일자, 기간, 보수의 2차원 리스트
-# 입력값 받기
-for idx in range(1, N + 1):
-    counsel_dates, counsel_pay = map(int, input().split())
-    word_lst[idx] = [idx, counsel_dates, counsel_pay]
-max_pay = 0
-# 일자별 다음 일자에 할 수 있는 모든 선택 따지기
-for choice_1st in range(1, N + 1):
+for i in range(N - 1, -1, -1):
+    if i + li[i][0] > N:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], li[i][1] + dp[i + li[i][0]])
+
+print(dp[0])
+
+'''
+재귀
+'''
+N = int(input())
+time_table = [list(map(int, input().split())) for _ in range(N)]
+
+def solve(i):
+    if i >= N:
+        return 0
+    ret = 0
+    if i + time_table[i][0] <= N:
+        ret = solve(i + time_table[i][0]) + time_table[i][1]
+    ret = max(ret, solve(i + 1))
+    return ret
+
+print(solve(0))
