f6dca104edde52ce24ce87f3d446e2bef11e71b2
minjeeki <minjeeki@student.42seoul.kr>
2024-10-11 11:09:47 +0900

    [BOJ 14889 스타트와 링크] 추가 코드 작성 (1140ms / 62868KB)

--- Weekly_solving/1011_BOJ_silver1_14889/minjee.py
+++ Weekly_solving/1011_BOJ_silver1_14889/minjee.py
@@ -34,3 +34,44 @@ team_l = []
 team_s = []
 ft_backtracking(0, 0, 0, 0, 0)
 print(min_gap)
+
+''' 기저조건에서 결과 구하는 코드 (1140ms / 62868KB)
+# 스타트 팀의 능력치와 링크 팀의 능력치의 차이 최소화
+def ft_backtracking(idx, cnt_team_l, cnt_team_s):
+    global min_gap
+    # 기저 조건 : 전체 팀 배정 완료 시
+    if idx == N:
+        tuple_l = tuple(team_l)
+        if tuple_l not in made_set:
+            made_set.add(tuple_l)
+            made_set.add(tuple(team_s))
+            total_l = 0
+            for item1 in team_l:
+                for item2 in team_l:
+                    total_l += powers[item1][item2]
+            total_s = 0
+            for item1 in team_s:
+                for item2 in team_s:
+                    total_s += powers[item1][item2]
+            min_gap = min(min_gap, abs(total_l - total_s))
+        return
+    # 재귀
+    if cnt_team_l < N // 2:
+        team_l.append(idx)
+        ft_backtracking(idx + 1, cnt_team_l + 1, cnt_team_s)
+        team_l.pop()
+    if cnt_team_s < N // 2:
+        team_s.append(idx)
+        ft_backtracking(idx + 1, cnt_team_l, cnt_team_s + 1)
+        team_s.pop()
+
+N = int(input())
+powers = [list(map(int, input().split())) for _ in range(N)]
+total = 0
+min_gap = 100 * N * N
+made_set = set()
+team_l = []
+team_s = []
+ft_backtracking(0, 0, 0)
+print(min_gap)
+'''
