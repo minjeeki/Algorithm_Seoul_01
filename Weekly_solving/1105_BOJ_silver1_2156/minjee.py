1918f62eeb67776c737bece5725b768c5e6c8e4c
minjeeki <minjeeki@student.42seoul.kr>
2024-11-05 11:06:46 +0900

    [BOJ 2156 포도주 시식] 코드 작성 (280ms, 31120KB)

--- Weekly_solving/1105_BOJ_silver1_2156/minjee.py
+++ Weekly_solving/1105_BOJ_silver1_2156/minjee.py
@@ -1,23 +1,17 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
-
-def dfs(i):
-    if memo[i] != -1:
-        return memo[i]
-    
-    if i == 0:
-        return wine[0]
-    elif i == 1:
-        return wine[0] + wine[1]
-    elif i == 2:
-        return max(wine[0] + wine[2], wine[1] + wine[2], wine[0] + wine[1])
-
-    memo[i] = max(dfs(i - 1), dfs(i - 2) + wine[i], dfs(i - 3) + wine[i - 1] + wine[i])
-    return memo[i]
-
 n = int(input())
 wine = [int(input()) for _ in range(n)]
 
-memo = [-1] * n
+if n == 1:
+    print(wine[0])
+elif n == 2:
+    print(wine[0] + wine[1])
+else:
+    dp = [0] * n
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+    dp[2] = max(wine[0] + wine[2], wine[1] + wine[2], dp[1])
 
-print(dfs(n - 1))
+    for i in range(3, n):
+        dp[i] = max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i])
+
+    print(dp[n - 1])
