9c214df3e5a7066551dd8af3c850ef71743010c9
minjeeki <minjeeki@student.42seoul.kr>
2024-11-08 04:47:28 +0900

    [BOJ 2529 부등호] 타인 코드 첨부 (120ms / 32140KB)
    리스트가 아닌 str을 이용했다는 점과  이로 인한 보다 효율적인 매개변수 처리가 인상적이었음

--- Weekly_solving/1108_BOJ_silver1_2529/minjee.py
+++ Weekly_solving/1108_BOJ_silver1_2529/minjee.py
@@ -35,3 +35,36 @@ num_lst = []
 
 dfs(-1, -1)
 print(answers[-1], answers[0], sep='\n')
+
+'''블로그 코드
+- 인상적이었던 점 : dfs의 매개변수 처리에서 num_lst를 통한 제어가 아닌 str을 이용할 수 있다는 것
+
+def check(a, b, op):
+  if op == '<':
+    if a > b: return False
+  if op == '>':
+    if a < b: return False
+  return True
+
+def dfs(cnt, num):
+  if cnt == k+1:
+    answer.append(num)
+    return
+  
+  for i in range(10):
+    if visited[i]: continue
+
+    if cnt == 0 or check(num[cnt-1], str(i), signs[cnt-1]):
+      visited[i] = 1
+      dfs(cnt+1, num+str(i))
+      visited[i] = 0
+
+k = int(input())
+signs = list(input().split())
+visited = [0]*10
+answer = []
+dfs(0, '')
+answer.sort()
+print(answer[-1])
+print(answer[0])
+'''
